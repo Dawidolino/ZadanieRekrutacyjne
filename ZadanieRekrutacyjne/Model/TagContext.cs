fb7c45b8eecb6be7baf4f7d21defd8bafa304d9c
Dawid Ruth <dawidolinp1@gmail.com>
2024-03-30 01:07:35 +0100

    add connection string

--- ZadanieRekrutacyjne/Model/TagContext.cs
+++ ZadanieRekrutacyjne/Model/TagContext.cs
@@ -5,6 +5,6 @@ namespace ZadanieRekrutacyjne.Model
     public class TagContext : DbContext
     {
         public TagContext(DbContextOptions<TagContext> options) : base(options) { }
-        public DbSet<Tag> Tags { get; set; }    
+        public DbSet<Tag> Tags { get; set; }        
     }
 }
