17209606b7ce576a0805584070d4a0fc76686753
Dawid Ruth <dawidolinp1@gmail.com>
2024-03-30 02:31:18 +0100

    add controller to get tags method

---
+++ ZadanieRekrutacyjne/Controllers/TagController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZadanieRekrutacyjne.Services;
+
+namespace ZadanieRekrutacyjne.Controllers
+{
+    [Route("api/gettags")]
+    [ApiController]
+    public class TagController : Controller
+    {
+        private readonly TagDownloader _tagDownloader;
+
+        public TagController(TagDownloader tagDownloader)
+        {
+            _tagDownloader = tagDownloader;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetTags()
+        {
+            try
+            {
+                await _tagDownloader.DownloadTagsFromStackOverflow();
+                return Ok("Tags downloaded successfully.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Failed to download tags: {ex.Message}");
+            }
+        }
+
+    }
+}
