80d4470fcd7433c604f13d6be6e7dab00e3b733d
Dawid Ruth <dawidolinp1@gmail.com>
2024-03-30 02:31:33 +0100

    modify tag.cs model

--- ZadanieRekrutacyjne/Model/Tag.cs
+++ ZadanieRekrutacyjne/Model/Tag.cs
@@ -2,9 +2,9 @@
 {
     public class Tag
     {
-        public int Id;
-        public string Name = "";
-        public int Count;
-        //public double Percentage; 
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int Count { get; set; }
+        public double Percentage { get; set; }
     }
 }
