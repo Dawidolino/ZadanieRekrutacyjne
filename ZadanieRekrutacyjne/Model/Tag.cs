dfded390ffb7751ff3aa6d5b4150403c6b1c51a0
Dawid Ruth <dawidolinp1@gmail.com>
2024-04-02 16:18:30 +0200

    clear version of the model

--- ZadanieRekrutacyjne/Model/Tag.cs
+++ ZadanieRekrutacyjne/Model/Tag.cs
@@ -3,8 +3,8 @@
     public class Tag
     {
         public int Id { get; set; }
-        public string Name { get; set; } = "";
         public int Count { get; set; }
+        public string Name { get; set; }
         public double Percentage { get; set; }
     }    
 }
