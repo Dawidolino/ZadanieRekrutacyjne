797da7ae7d7866eac62927a6503467fcf6cc2ae1
Dawid Ruth <dawidolinp1@gmail.com>
2024-04-03 15:50:59 +0200

    routing test

--- ZadanieRekrutacyjne/src/app/app.routes.ts
+++ ZadanieRekrutacyjne/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TagListComponent } from './tag-list/tag-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: TagListComponent }, // Default route for tag list
+{ path: 'tags', component: TagListComponent }, // Route for explicit tag list access
+];
